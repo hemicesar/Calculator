3854a3de016a89feb9d959f48b8be14d19e9be57
mariferzu <marifergarceszu@gmail.com>
2025-10-16 20:20:15 -0600

    Mi primer commit

--- fer.py
+++ fer.py
@@ -1,1 +1,11 @@
 #Cambios y aportaciones de Fer
+#caluladora
+ #Cambios y aportaciones de Fer
+class Operator:
+    def __init__(self):
+        pass
+    def resta(self,x,y):
+        return x - y
+    def divicion (self, x,y):
+        return x / y
+       
