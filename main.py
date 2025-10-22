508d5a10c7a8fd0dfa13c71dbcd8ff0481f06f87
Moooy <moyenci@gmail.com>
2025-10-21 21:30:50 -0600

    ecuación cuadrática

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Codificado en Python
 # Coders:  Fer and Moy
 # Date October 14th
-
+import math
 from fer import Operador
 from moy import Operators
 
@@ -15,7 +15,7 @@ def main():
     try:
         num1 = float(input("Indique su primer número: "))
         num2 = float(input("Indique su segundo número: "))
-        operation = input("Indique la operación a realizar: [ +, -, *, / ]")
+        operation = input("Indique la operación a realizar: [ +, -, *, / , sqrt, x**1, x**2]: ")
 
         if operation == '+': 
             resultado = operadores.suma(num1, num2)
@@ -25,6 +25,14 @@ def main():
             resultado = operadores.multiplicacion(num1, num2)
         elif operation == '/': 
             resultado = operador.divicion(num1, num2)
+        elif operation == 'sqrt':
+            resultado = operadores.raiz_cuadrada(num1)
+        elif operation == 'x**1':
+            num3 = float(input("Indique el valor de num3: "))
+            resultado = operadores.ecuacion_lineal(num1, num2, num3) ### num1*x + num2 = num3
+        elif operation == 'x**2':
+            num3 = float(input("Indique el valor de num3: "))
+            resultado = operadores.ecuacion_cuadratica(num1, num2, num3) ### num1*x^2 + num2*x + num3 = 0
         else:
             resultado = "Operación no codificada aún!"
 
