35ab690c0ce18710fcfac72dc7035e5fc5cd07de
hemicesar <he.mi.cesar@gmail.com>
2025-11-25 21:13:26 -0600

    script de conversiones, añadido

---
+++ conversion.py
@@ -0,0 +1,9 @@
+lib = 2.2049
+
+kg = float(input("Ingrese la cantidad de kilogramos a convertir"))
+resultado = kg*lib
+print (f"{resultado} lib")
+
+libra = float(input("Ingrese la cantidad de libras a convertir"))
+resultado_2 = libra/lib
+print (f"{resultado_2} kg")
